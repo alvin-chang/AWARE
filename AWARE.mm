c5ba8698d324685fb78c92cc1b9d0bf08164fd0b
Alvin Chang <alvin.chang@gmail.com>
2013-04-02 10:59:18 +0100

    initial mind map

---
+++ AWARE.mm
@@ -0,0 +1,24 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1364545684723" ID="ID_901535189" MODIFIED="1364892415106" TEXT="Autonomous&#xa;Warehouse&#xa;Automated&#xa;Resource&#xa;Engine">
+<font NAME="SansSerif" SIZE="20"/>
+<icon BUILTIN="idea"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1364545684723" ID="ID_1557943476" MODIFIED="1364889907332" POSITION="left" TEXT="Queen">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<attribute NAME="Ability" VALUE="Command"/>
+<attribute NAME="Ability" VALUE="Spawn"/>
+<attribute NAME="Ability" VALUE="Demote"/>
+<attribute NAME="Attribute" VALUE="Goal"/>
+<node COLOR="#00b439" CREATED="1364545883101" ID="ID_591270484" MODIFIED="1364547249196" TEXT="Worker">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<attribute NAME="Ability" VALUE="Obey"/>
+<attribute NAME="Ability" VALUE="Vote"/>
+<attribute NAME="Ability" VALUE="Promote"/>
+<attribute NAME="Attribute" VALUE="Role"/>
+</node>
+</node>
+</node>
+</map>
